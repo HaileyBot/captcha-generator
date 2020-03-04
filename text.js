e634748b1e34b2cedde0699cdacc46369450bcb4
Quin <cheesits456@haileybot.com>
2020-03-03 18:54:33 -0800

    first commit

---
+++ text.js
@@ -0,0 +1,79 @@
+"use strict";
+
+const fs = require("fs"),
+  path = require("path"),
+  Canvas = require("canvas");
+  
+const alternateCapitals = str => [...str].map((char, i) => char[`to${i % 2 ? "Upper" : "Lower"}Case`]()).join(""),
+  randomText = () => alternateCapitals(Math.random().toString(36).replace(/[^a-z]+/gi, "").substring(0, 6));
+  
+class Captcha {
+  constructor() {
+    
+    // Initialize canvas
+    this._canvas = Canvas.createCanvas(200, 200);
+    let ctx = this._canvas.getContext('2d');
+
+    // Set background color
+    ctx.globalAlpha = 1;
+    
+    // Set style for lines
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = 2;
+    
+    // Draw 8 lines
+    for (let i = 0; i < 8; i++) {
+
+      // Initialize coordinate array
+      let points = [];
+      
+      // Set X and Y of first coordinate
+      points[0] = [
+        25,
+        Math.round(Math.random() * 150) + 25
+      ];
+
+      // Set X and Y of second coordinate
+      points[1] = [
+        200 - 25,
+        Math.round(Math.random() * 150) + 25
+      ];
+
+      // Draw first 4 lines horizontal
+      if (i < 4) {
+        ctx.moveTo(points[0][0], points[0][1]);
+        ctx.lineTo(points[1][0], points[1][1]);
+      // Draw last 4 lines vertical
+      } else {
+        ctx.moveTo(points[1][1], points[1][0]);
+        ctx.lineTo(points[0][1], points[0][0]);
+      }
+    };
+    // Fill all the plotted line strokes
+    ctx.stroke();
+
+    // Set style and position for text
+    ctx.font = 'normal 40px serif';
+    ctx.textAlign="center";
+    ctx.textBaseline="middle";
+    ctx.fillStyle = '#000';
+    ctx.translate(100, 100);
+    ctx.rotate((Math.random() - 0.5) * 2);
+
+    // Set text value and print it to canvas
+    this._value = randomText();
+    ctx.fillText(this._value, 0, 0);
+
+  };
+  
+  get value() {
+    return this._value;
+  }
+
+  get canvas() {
+    return this._canvas;
+  }
+  
+}
+
+(new Captcha()).canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'text.png')));
