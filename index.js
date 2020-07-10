7f0a2c6e126b8ab1211da6e74fbb1c4819f496b1
Quin <quin@cheesits456.dev>
2020-07-10 00:38:49 -0700

    increased resolution, made it harder for AI to solve

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ class Captcha {
   constructor() {
 
     // Initialize canvas
-    this._canvas = Canvas.createCanvas(200, 200);
+    this._canvas = Canvas.createCanvas(400, 400);
     let ctx = this._canvas.getContext('2d');
 
     // Set background color
     ctx.globalAlpha = 1;
     ctx.fillStyle = "white";
     ctx.beginPath();
-    ctx.fillRect(0, 0, 200, 200);
+    ctx.fillRect(0, 0, 400, 400);
 
     // Draw background noise
     for (let i = 0; i < 10000; i++) {
@@ -42,8 +42,8 @@ class Captcha {
       while (color.length < 7) color += Math.round(Math.random() * 16).toString(16);
       ctx.fillStyle = color;
       ctx.arc(
-        Math.round(Math.random() * 200), // X coordinate
-        Math.round(Math.random() * 200), // Y coordinate
+        Math.round(Math.random() * 400), // X coordinate
+        Math.round(Math.random() * 400), // Y coordinate
         Math.random(), // Radius
         0, // Start angle
         Math.PI * 2 // End angle
@@ -52,16 +52,16 @@ class Captcha {
     }
 
     // Set style for circles
-    ctx.fillStyle = "#555";
+    ctx.fillStyle = "#222";
     ctx.lineWidth = 0;
 
     // Draw 80 circles
-    for (let i = 0; i < 80; i++) {
+    for (let i = 0; i < 150; i++) {
       ctx.beginPath();
       ctx.arc(
-        Math.round(Math.random() * 180) + 10, // X coordinate
-        Math.round(Math.random() * 180) + 10, // Y coordinate
-        Math.round(Math.random() * 7), // Radius
+        Math.round(Math.random() * 360) + 20, // X coordinate
+        Math.round(Math.random() * 360) + 20, // Y coordinate
+        Math.round(Math.random() * 8), // Radius
         0, // Start angle
         Math.PI * 2 // End angle
       );
@@ -70,14 +70,14 @@ class Captcha {
 
     // Set style for lines
     ctx.strokeStyle = "#222";
-    ctx.lineWidth = 2;
+    ctx.lineWidth = 4;
 
     // Draw 10 lines
     ctx.beginPath();
     let coords = [];
     for (let i = 0; i < 4; i++) {
       if (!coords[i]) coords[i] = [];
-      for (let j = 0; j < 5; j++) coords[i][j] = Math.round(Math.random() * 40) + (j * 40);
+      for (let j = 0; j < 5; j++) coords[i][j] = Math.round(Math.random() * 80) + (j * 80);
       if (!(i % 2)) coords[i] = shuffleArray(coords[i]);
     }
 
@@ -86,10 +86,10 @@ class Captcha {
         for (let j = 0; j < coords[i].length; j++) {
           if (!i) {
             ctx.moveTo(coords[i][j], 0);
-            ctx.lineTo(coords[i + 1][j], 200);
+            ctx.lineTo(coords[i + 1][j], 400);
           } else {
             ctx.moveTo(0, coords[i][j]);
-            ctx.lineTo(200, coords[i + 1][j]);
+            ctx.lineTo(400, coords[i + 1][j]);
           }
         }
       }
@@ -99,27 +99,27 @@ class Captcha {
     ctx.stroke();
 
     // Set style for text
-    ctx.font = 'bold 10px sans';
+    ctx.font = 'bold 20px sans';
     ctx.fillStyle = 'blue';
 
     // Set position for text
     ctx.textAlign = "left";
     ctx.textBaseline = "bottom";
-    ctx.translate(0, 200);
+    ctx.translate(0, 400);
 
     // Set text value and print it to canvas
     ctx.beginPath();
     ctx.fillText("http://c456.xyz/captcha-generator", 0, 0);
 
     // Set style for text
-    ctx.font = 'bold 45px swift';
+    ctx.font = 'bold 90px swift';
     ctx.fillStyle = '#222';
 
     // Set position for text
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
-    ctx.translate(0, -200);
-    ctx.translate(Math.round((Math.random() * 50) - 25) + 100, Math.round((Math.random() * 50) - 25) + 100);
+    ctx.translate(0, -400);
+    ctx.translate(Math.round((Math.random() * 100) - 50) + 200, Math.round((Math.random() * 100) - 50) + 200);
     ctx.rotate(Math.random() - 0.5);
 
     // Set text value and print it to canvas
