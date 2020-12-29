97e4ba23d463d56db70c517e07ef54c5b3db9f80
cheesits456 <quin@cheesits456.dev>
2020-12-29 05:15:34 -0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,36 +3,59 @@ const assert = require("assert");
 const Canvas = require("canvas");
 const Captcha = require("../");
 
-describe("Captcha", function () {
-	it("should export function 'captcha'", function () {
-		assert.strictEqual(typeof Captcha, "function");
-	});
+describe("src/index.ts", function () {
+	describe("Captcha", function () {
+		it("should export function 'captcha'", function () {
+			assert.strictEqual(typeof Captcha, "function");
+		});
 
-	const captcha = new Captcha();
-	describe("captcha", function () {
-		it("should export an object", function () {
-			assert.strictEqual(typeof captcha, "object");
-		});
-		describe("value", function () {
-			it("should be a string", function () {
-				assert.strictEqual(typeof captcha.value, "string");
+		const captcha = new Captcha();
+		describe("captcha", function () {
+			it("should export an object", function () {
+				assert.strictEqual(typeof captcha, "object");
 			});
-			it("should be 6 characters long", function () {
-				assert.strictEqual(captcha.value.length, 6);
+
+			describe("value", function () {
+				it("should be a string", function () {
+					assert.strictEqual(typeof captcha.value, "string");
+				});
+				it("should be 6 characters long", function () {
+					assert.strictEqual(captcha.value.length, 6);
+				});
+				it("should only contain letters", function () {
+					assert.match(captcha.value, /^[a-z]+$/i);
+				});
 			});
-			it("should only contain letters", function () {
-				assert.match(captcha.value, /^[a-z]+$/i);
+
+			describe("PNGStream", function () {
+				it("should be an instance of canvas PNG Stream", function () {
+					assert.strictEqual(captcha.PNGStream instanceof Canvas.PNGStream, true);
+				});
 			});
-		});
-		describe("PNGStream", function () {
-			it("should be an instance of canvas PNG Stream", function () {
-				assert.strictEqual(captcha.PNGStream instanceof Canvas.PNGStream, true);
-			});
-		});
-		describe("JPEGStream", function () {
-			it("should be an instance of canvas JPEG Stream", function () {
-				assert.strictEqual(captcha.JPEGStream instanceof Canvas.JPEGStream, true);
+
+			describe("JPEGStream", function () {
+				it("should be an instance of canvas JPEG Stream", function () {
+					assert.strictEqual(captcha.JPEGStream instanceof Canvas.JPEGStream, true);
+				});
 			});
 		});
 	});
+});
+
+const fs = require("fs");
+const path = require("path");
+require("../examples/writeToFiles");
+describe("examples/writeToFiles.js", function () {
+	const files = fs
+		.readdirSync(path.join(__dirname, "..", "examples"))
+		.filter(f => ["png", "jpeg"].includes(f.split(".")[1]));
+	it("should create a new PNG file", function () {
+		assert.strictEqual(Boolean(files.filter(f => f.endsWith(".png")).length), true);
+	});
+	it("should create a new JPEG file", function () {
+		assert.strictEqual(Boolean(files.filter(f => f.endsWith(".jpeg")).length), true);
+	});
+	for (const file of files) {
+		fs.unlinkSync(path.join(__dirname, "..", "examples", file));
+	}
 });
